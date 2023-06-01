8e934e77d3dfb47489a3e6348d0d96cb6b13f349
Eugene-Lasevich <lasevich009@gmail.com>
2023-06-01 23:52:42 +0300

    Create basic pages

---
+++ myshop/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name='shop'
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(('shop.urls','shop'), namespace='shop')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
